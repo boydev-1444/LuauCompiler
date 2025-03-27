c291820336e4a89d8f9465252b85be01405b79b1
boydev1444 <cherm5754@gmail.com>
2025-03-26 19:53:18 -0600

    Delete 2

--- netlify/functions/hallo.js
+++
@@ -1,9 +0,0 @@
-const { createResponse } = require('@netlify/functions');
-
-exports.handler = async (event, context) => {
-  const responseMessage = {
-    message: "¡Hola desde la función serverless de Netlify!",
-  };
-
-  return createResponse(200, responseMessage);
-};
